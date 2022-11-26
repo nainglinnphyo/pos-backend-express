d3b3e934ac5e74159a9c39971a3257cb07348858
nainglinnphyo <nainglinnphyo24@gmail.com>
2022-11-26 11:59:07 +0630

    update

--- src/product/product.routes.ts
+++ src/product/product.routes.ts
@@ -6,6 +6,6 @@ productRouter.post("/create", productController.createProduct);
 productRouter.get("/fetch", productController.fetchProduct);
 productRouter.post("/create-price-list", productController.createProductPriceList);
 productRouter.put("/edit", productController.editProduct);
-productRouter.put("/fetch-product-price-list", productController.fetchProductPriceList);
+productRouter.get("/fetch-product-price-list", productController.fetchProductPriceList);
 
 export default productRouter;
